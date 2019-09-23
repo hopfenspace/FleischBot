f862f9f7dce33def98904275c7c719b7beaaa5a0
unasmera <tabea.easybox@gmail.com>
2019-09-23 23:21:33 +0200

    Added random response to command "Fleisch"

--- FleischBot.py
+++ FleischBot.py
@@ -4,10 +4,21 @@ from telegram.ext import Updater, CommandHandler
 with open ("Fleisch.json", "r+") as fd:
     Kontos=json.load(fd)
 
+FleischMessages = [
+	"Und schon wieder ein totes Tier...",
+	"Der Meeresspiegel steigt! 🌊",
+	"Iss auch mal einen Salat!",
+	"🐷 REDRUM",
+	"10 years from now... - Remember how we used to have snow in the winter? 😔",
+    "20 years from now... - Remember how we were able to drink water from the sink? 💧",
+    "50 years from now... - grandpa/ma, what's a snowman? ☃️",
+    "100 years fro-; OH, never mind, you died :-) 🌄"
+]
+
 def Fleisch(bot,update):
     id=update.message.from_user.id
     id=str(id)
-    update.message.reply_text("Und schon wieder ein totes Tier...")
+    update.message.reply_text(random.choice(FleischMessages)
     if id in Kontos:
         Kontos[id] = Kontos[id]+1
         with open ("Fleisch.json", "w") as fd:
@@ -36,7 +47,7 @@ def SetzeNull(bot,update):
     with open ("Fleisch.json", "w") as fd:
             json.dump(Kontos,fd)
 
-updater = Updater('860123473:AAFWU0FHzrcpOKCaS73KD5sTc67mQjX-EAg')
+updater = Updater('Add your token')
 dp = updater.dispatcher
 dp.add_handler(CommandHandler('Fleisch',Fleisch))
 dp.add_handler(CommandHandler('Fleischverbrauch',Fleischverbrauch))
